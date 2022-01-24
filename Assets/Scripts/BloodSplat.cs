fc0f479a4f47ceb17b8570e82ee269c175ba55ab
adavis1989 <83476184+adavis1989@users.noreply.github.com>
2022-01-23 19:43:35 -0600

    Added final Assets

--- Assets/Scripts/BloodSplat.cs
+++ Assets/Scripts/BloodSplat.cs
@@ -7,7 +7,7 @@ public class BloodSplat : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(this.gameObject, 1.5f);
+        Destroy(this.gameObject, 1f);
     }
 
 }
